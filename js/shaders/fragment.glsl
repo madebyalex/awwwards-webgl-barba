adfe1950a7f96bf1a3580845453cdb33dc0216a3
Alexander <hello@alexander.works>
2021-08-05 23:31:14 +0300

    Flickering & multi-color  wavy plain

--- js/shaders/fragment.glsl
+++ js/shaders/fragment.glsl
@@ -1,5 +1,8 @@
+uniform float time;
+
 varying float pulse;
+varying vec2 vUv;
 
 void main() {
-  gl_FragColor = vec4( 1.,pulse,0.5,1.);
+  gl_FragColor = vec4( vUv.x,pulse * sin(time * 10.),vUv.y,1.);
 }
--- js/shaders/vertex.glsl
+++ js/shaders/vertex.glsl
@@ -1,5 +1,7 @@
 uniform float time;
+
 varying float pulse;
+varying vec2 vUv;
 
 void main() {
   vec3 newPosition = position;
@@ -7,4 +9,6 @@ void main() {
   newPosition.z = 0.05 * sin(length(position) * 30. + time);
   pulse = newPosition.z * 20.;
   gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+
+  vUv = uv;
 }
