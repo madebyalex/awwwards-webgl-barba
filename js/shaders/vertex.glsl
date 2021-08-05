fa7aacc36f0f9e739fa0abc4159e18dcd5892bb8
Alexander <hello@alexander.works>
2021-08-05 23:20:34 +0300

    Jellyfish-like plane

--- js/shaders/vertex.glsl
+++ js/shaders/vertex.glsl
@@ -6,5 +6,5 @@ void main() {
   // newPosition.z = 0.05 * sin(newPosition.x * 30.   + time);
   newPosition.z = 0.05 * sin(length(position) * 30. + time);
   pulse = newPosition.z * 20.;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
 }
