54e3721f1b3c3c46cff667b89ca2eede2c9626ff
Alexander <hello@alexander.works>
2021-08-08 02:39:03 +0300

    Simple full-page transition

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ import vertex from './shaders/vertex.glsl';
 import testTexture from 'url:../texture.jpg';
 import * as dat from 'dat.gui';
 import { DoubleSide } from 'three';
+import gsap from 'gsap';
 
 export default class Sketch {
   constructor(options) {
@@ -65,7 +66,7 @@ export default class Sketch {
   addObjects() {
     // this.geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5, 75, 75, 20);
     // this.geometry = new THREE.CylinderBufferGeometry(0.2, 0.2, 0.5, 40, 20);
-    this.geometry = new THREE.PlaneBufferGeometry(250, 250);
+    this.geometry = new THREE.PlaneBufferGeometry(250, 250, 100, 100);
     // this.geometry = new THREE.SphereBufferGeometry(0.3, 120, 120);
 
     // this.material = new THREE.MeshNormalMaterial();
@@ -77,12 +78,26 @@ export default class Sketch {
         uProgress: { value: this.settings.progress },
         uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         uTextureSize: { value: new THREE.Vector2(100, 100) },
+        uCorners: { value: new THREE.Vector2(0, 0) },
         uResolution: { value: new THREE.Vector2(this.width, this.height) },
         uQuadSize: { value: new THREE.Vector2(250, 250) },
       },
       vertexShader: vertex,
       fragmentShader: fragment,
     });
+
+    this.tl = gsap
+      .timeline()
+      .to(this.material.uniforms.uCorners.value, {
+        x: 1,
+      })
+      .to(
+        this.material.uniforms.uCorners.value,
+        {
+          y: 1,
+        },
+        0.3
+      );
 
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
@@ -93,7 +108,8 @@ export default class Sketch {
   render() {
     this.time += 0.05;
     this.material.uniforms.time.value = this.time;
-    this.material.uniforms.uProgress.value = this.settings.progress;
+    // this.material.uniforms.uProgress.value = this.settings.progress;
+    this.tl.progress(this.settings.progress);
 
     this.mesh.rotation.x = this.time / 2000;
     this.mesh.rotation.y = this.time / 1000;
--- js/shaders/vertex.glsl
+++ js/shaders/vertex.glsl
@@ -3,6 +3,7 @@ uniform float uProgress;
 uniform sampler2D uTexture;
 uniform vec2 uResolution;
 uniform vec2 uQuadSize;
+uniform vec4 uCorners;
 
 varying vec2 vUv;
 varying vec2 vSize;
@@ -13,7 +14,9 @@ void main() {
   fullScreenState.x *= uResolution.x / uQuadSize.x;
   fullScreenState.y *= uResolution.y / uQuadSize.y;
 
-  vec4 finalState = mix(defaultState, fullScreenState, uProgress);
+  float cornersProgress = mix(uCorners.x, uCorners.y, uv.x);
+
+  vec4 finalState = mix(defaultState, fullScreenState, cornersProgress);
 
   vSize = mix(uQuadSize, uResolution, uProgress);
 
